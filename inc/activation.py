3db7c5e71c03256c564c5dcf47e7c40c09a30f38
kpetridis <petridkon@gmail.com>
2022-08-11 13:00:51 +0300

    Delete inc directory

--- inc/activation.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def ReLU(x):
-    return np.maximum(0, x)
--- inc/cost.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-def cost_function(y_model, y_target, method="mse"):
-    return sum((y_target[i, j] - y_model[i, j]) ** 2 for i in range(y_model.shape[0]) for j in range(y_model.shape[1]))
